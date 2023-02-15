1f12fb44322a7393d7ab1497a2c5fc8224d4fe4e
10xyz <82887902+10xyz-coder@users.noreply.github.com>
2023-02-15 20:22:06 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Belua'
+copyright = '2023, 10xyz'
+author = '10xyz'
 
 release = '0.1'
-version = '0.1.0'
+version = '1.0.0'
 
 # -- General configuration
 
